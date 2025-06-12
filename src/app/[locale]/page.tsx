ad9d449e5ae4c597a4b6dc3b54f438afa36b89aa
İnanç CAN <ge82ced@mytum.de>
2025-06-12 16:01:20 +0200

    refactor: remove unused Sanity imports and iframe section from HomePage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,10 +2,7 @@ import {Locale} from 'next-intl';
 import {setRequestLocale} from 'next-intl/server';
 import {use} from 'react';
 import HomePage from '@/components/HomePage';
-import { type SanityDocument } from "next-sanity";
-import { client } from "@/sanity/client";
 
-const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
 
 
 type Props = {
