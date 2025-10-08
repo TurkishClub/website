5b91da2dda7147e65e07fbcf3fd0c052f99d594a
Meric Feyzullahoglu <meric.feyzullahoglu@gmail.com>
2025-10-08 23:14:34 +0200

    Add study place search under the tools menu in navbar
    Signed-off-by: Meric Feyzullahoglu <meric.feyzullahoglu@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import {
   MessageCircle,
   Search,
   BookOpen,
+  NotebookPen,
   Users,
   ChevronDown,
   Calendar,
@@ -218,6 +219,12 @@ export default function Navbar() {
                         label: t('dormSearch'),
                         desc: t('dormSearchDesc'),
                         icon: <Search className="h-4 w-4 text-blue-500" />
+                      },
+                      {
+                        href: '/ders-yeri-arama',
+                        label: t('studyPlaceSearch'),
+                        desc: t('studyPlaceSearchDesc'),
+                        icon: <NotebookPen className="h-4 w-4 text-green-500" />
                       }
                     ].map((opt, index) => (
                       <motion.div
